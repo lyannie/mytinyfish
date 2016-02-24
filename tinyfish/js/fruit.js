034e060616a39459657a22612d040638078d4660
lyannie <liyang.annie@gmail.com>
2016-02-24 20:27:36 +0800

    add js

---
+++ tinyfish/js/fruit.js
@@ -0,0 +1,12 @@
+var fruitObj = function  () {
+	this.alive = []; //bool;
+}
+fruitObj.prototype.num = 30;
+fruitObj.prototype.init = function(){
+	for (var i = 0; i < this.num; i++) {
+		this.alive[i] = false;
+	}
+}
+fruitObj.prototype.draw = function(){
+	
+}
---
+++ tinyfish/js/main.js
@@ -0,0 +1,48 @@
+var can1;
+var can2;
+
+var ctx1;
+var ctx2;
+
+var lastTime;
+var deltaTime;
+
+var bgPic = new Image();
+
+var canWidth;
+var canHeight;
+
+var ane;
+
+document.body.onload = game;
+function game(){
+	init();
+	lastTime = Date.now();
+	deltaTime = 0;
+	gameloop();
+}
+function init(){
+	//get canvas context;
+	can1 = document.getElementById("canvas1");//fishes,dust,UI,circle;
+	ctx1 = can1.getContext("2d");
+	can2 = document.getElementById("canvas2");//background,ane,fruits;
+	ctx2 = can2.getContext("2d");
+
+	bgPic.src = "./src/background.jpg";
+
+	canWidth = can1.width;
+	canHeight = can1.height;
+
+	ane = new aneObj();
+	ane.init();
+
+}
+function gameloop(){
+	window.requestAnimFrame(gameloop); //setInterval, setTimeout, frame per second;
+	var now = Date.now();
+	deltaTime = now - lastTime;
+	lastTime = now;
+
+ 	drawBackground();
+ 	ane.draw();
+}
